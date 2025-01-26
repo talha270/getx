12cea8f33e65b2f195b8e71ae6b5e2598b81b7cd
talha270 <144779558+talha270@users.noreply.github.com>
2025-01-26 17:19:19 +0500

    added

---
+++ lib/depencies_injection/example_one.dart
@@ -0,0 +1,75 @@
+import 'counter_controller.dart';
+import 'package:get/get.dart';
+
+
+class CounterController extends GetxController {
+  // Observable variable
+  var count = 0.obs;
+
+  // Method to increment the counter
+  void increment() {
+    count++;
+  }
+}
+
+
+
+class CounterBinding extends Bindings {
+  @override
+  void dependencies() {
+    // Injecting CounterController lazily
+    Get.lazyPut<CounterController>(() => CounterController());
+  }
+}
+
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Access the injected CounterController
+    final CounterController controller = Get.find();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter Page')),
+      body: Center(
+        child: Obx(
+              () => Text(
+            'Count: ${controller.count}',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'counter_page.dart';
+import 'counter_binding.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/counter',
+      getPages: [
+        GetPage(
+          name: '/counter',
+          page: () => CounterPage(),
+          binding: CounterBinding(), // Attach the binding here
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// Controller for managing the counter
+class CounterController extends GetxController {
+  var count = 0.obs; // Observable variable
+
+  void increment() {
+    count++;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // Injecting the controller using Get.put
+  final CounterController controller = Get.put(CounterController());
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(  // Use GetMaterialApp instead of MaterialApp
+      title: 'GetX Example',
+      initialRoute: '/',    // Initial route
+      getPages: [
+        GetPage(name: '/', page: () => HomePage()),
+        GetPage(name: '/next', page: () => NextPage()),
+      ],
+    );
+  }
+}
+
+// Home Page
+class HomePage extends StatelessWidget {
+  final CounterController controller = Get.find();  // Retrieve the controller
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Obx(() => Text(
+          'Count: ${controller.count}',
+          style: TextStyle(fontSize: 25),
+        )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.increment();  // Increment the count
+        },
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: ElevatedButton(
+          onPressed: () {
+            Get.toNamed('/next');  // Navigate to the NextPage
+          },
+          child: Text('Go to Next Page'),
+        ),
+      ),
+    );
+  }
+}
+
+// Next Page
+class NextPage extends StatelessWidget {
+  final CounterController controller = Get.find();  // Retrieve the same controller
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Next Page')),
+      body: Center(
+        child: Obx(() => Text(
+          'Count on Next Page: ${controller.count}',
+          style: TextStyle(fontSize: 25),
+        )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.back();  // Navigate back to the HomePage
+        },
+        child: Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
